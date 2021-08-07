a5d86d4a80e047c30a31c59e4433ab00b298fe84
aoi-b <aoi_zono@mail.benesse.co.jp>
2021-08-07 15:48:56 +0900

    #43 音楽の読み込みテスト4

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-/** BGM */
+/** 音楽 */
 let bgm = new Audio();
+let soundEffect = new Audio();
 
 /** 暗転を見せる関数 */
 const showDarkness = () => {
@@ -7,17 +8,30 @@ const showDarkness = () => {
     $('#darkness-wrapper').delay(500).show().animate({'opacity': 1}, 500, 'swing', () => {
         // 暗転中に場面を切り替える
         $('#title-wrapper').show();
-        $('#button-wrapper').show();
+        $('#ok-button').hide();
+        $('#start-button').show();
         $('#nyateracy-wrapper').show();
-        $('#logo-wrapper').hide();
+        $('#caution-wrapper').hide();
         $('body').css('backgroundImage', "url(./img/background/top_title.png)");
         // 明転
         $('#darkness-wrapper').animate({'opacity': 0}, 500, 'swing', () => {
             $('#darkness-wrapper').hide();
-            // $('#bgm').get(0).volume = 0.2;
-            // $('#bgm').get(0).play();
             bgm.volume = 0.2;
             bgm.play();
+        });
+    })
+}
+
+const showCaution = () => {
+    // 暗転
+    $('#darkness-wrapper').delay(500).show().animate({'opacity': 1}, 500, 'swing', () => {
+        // 暗転中に場面を切り替える
+        $('#caution-wrapper').show();
+        $('#logo-wrapper').hide();
+        $('#ok-button').show();
+        // 明転
+        $('#darkness-wrapper').animate({'opacity': 0}, 500, 'swing', () => {
+            $('#darkness-wrapper').hide();
         });
     })
 }
@@ -25,34 +39,30 @@ const showDarkness = () => {
 /** ローディングを見せる関数 */
 const loading = () => {
     $('#title-wrapper').hide();
-    $('#button-wrapper').hide();
     $('#nyateracy-wrapper').hide();
     $('#logo-wrapper').show();
+    $('#caution-wrapper').hide();
+    $('#ok-button').hide();
+    $('#start-button').hide();
     bgm.src = "./music/bgm/so_sweet.mp3";
+    soundEffect.src = "./music/sound_effect/「ピロリ」決定のボタン音・アクセント音.mp3";
     $('#loading').delay(800).fadeOut(1000, () => {
-        showDarkness();
+        showCaution();
     });
-}
-
-/** 効果音を鳴らす関数 */
-const makeSound = (sound) => {
-    if (sound === "") {
-        $('#sound-effect source').attr('src', ObjectData.soundEffect.forward);
-    }
-    if (sound === "click") {
-        $('#sound-effect source').attr('src', ObjectData.soundEffect.click);
-    }
-    document.querySelector("#sound-effect").load();
-    $('#sound-effect').get(0).play();
 }
 
 /** ページを読み込む */
 $(window).on('load', loading());
 
 /** スタートボタンを押す */
+$('#ok-button').on("click", () => {
+    showDarkness();
+});
+
+/** スタートボタンを押す */
 $('#start-button').on("click", () => {
-    $('#sound-effect').get(0).volume = 0.4;
-    $('#sound-effect').get(0).play();
+    soundEffect.volume = 0.4;
+    soundEffect.play();
     $('#darkness-wrapper').show().animate({'opacity': 1}, 1000, 'swing', () => {
         window.location.href = './html/conversation1_1.html';
     });
